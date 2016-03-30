58dd7d7eb8e22cb47314ed3cf82f542fbbd75eae
abeagomez <abeagomez@gmail.com>
2016-03-30 17:03:32 -0500

    Clean project: first commit

---
+++ Assets/CircleRunSlow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleRunSlow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 1.5f, 0));
+    }
+}
